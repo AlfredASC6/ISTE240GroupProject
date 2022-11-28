daab15f763238525893a932984eef2966ceb5ced
AlfredASC6 <alfredfrac99@gmail.com>
2022-11-28 11:55:33 -0500

    some files I been working on during break

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css" type = "text/css">
+    <title>Deaf History</title>
+</head>
+<body>
+    <nav class = "nav">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="thomas.html">Thomas Gallaudet</a></li>
+            <li><a href="alice.html">Alice Cogswell</a></li>
+            <li><a href="dimarco.html">Nyle Di Marco</a></li>
+            <li><a href="laurent.html">Laurent Clerc</a></li>
+            <li><a href="feedback.html">Feedback</a></li>
+        </ul>
+    </nav>
+    <br><br><br>
+    <section id = "center">
+
+        <h1 class = "title">Deaf History</h1>
+
+        <section id = "mid-holder">
+            <div id = "left-holder">
+                 this is where the video will go, replace text once we find the video
+            </div>
+            <p id = "text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis vitae fugiat non quae dolore odit laudantium
+                 temporibus ipsa enim. Maxime praesentium dolores aperiam beatae error deserunt natus aut mollitia rerum?
+            </p>
+        </section>
+        
+    </section>
+
+    <section id = "bottom">
+        <p id = "desc">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo nisi reprehenderit veniam consectetur soluta 
+            molestiae doloribus ipsum, sunt commodi molestias sit non, adipisci ducimus tempore odio voluptatum doloremque 
+            possimus delectus!
+        </p>
+
+        <section id = "stat"> this is where the statistical image will go in</section>
+    </section>
+</body>
+</html>
