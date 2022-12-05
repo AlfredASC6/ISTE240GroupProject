edeb87377353a598665658cd912436445ef7038a
AlfredASC6 <alfredfrac99@gmail.com>
2022-12-05 18:15:11 -0500

    updated the nav bar

--- ISTE240GroupProject/inc/nav.php
+++ ISTE240GroupProject/inc/nav.php
@@ -5,8 +5,8 @@
         <a href="alice.php">Alice Cogswell</a>
         <a href="dimarco.php">Nyle Di Marco</a>
         <a href="laurent.php">Laurent Clerc</a>
-        <a href="feedback.php">Feedback</a>
         <a href="keller.php">Helen Keller</a>
+        <a href="../form/comments.php">Feedback</a>
         <a href="javascript:void(0);" class="icon" onclick="myFunction()"> <i class="fa fa-bars"></i> </a>
     </ul>
   </nav>
