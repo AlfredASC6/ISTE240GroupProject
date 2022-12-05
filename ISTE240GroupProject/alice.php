71dc6934e2749a4c9db584ed8d70f5f9bd739257
ajs2019 <118772040+ajs2019@users.noreply.github.com>
2022-12-04 19:15:11 -0500

    Add files via upload

---
+++ ISTE240GroupProject/alice.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="asset/css/styles.css" type = "text/css">
+<link rel="stylesheet" href="asset/css/nav.css" type = "text/css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="asset/js/javascript.js"></script>
+<title>Thomas</title>
+
+
+
+</head>
+<body>
+<?php include "inc/nav.php"; ?>
+
+
+    <header>  
+        <h1>Alice Cogswell</h1>
+    
+    </header>
+
+<div class="row">
+
+
+    <main>
+
+     <div class="image" style="height:200px;">Image</div>
+
+
+
+     <br>
+
+
+
+     <div class="image" style="height:200px;">Image</div>
+
+
+
+    </main>
+    <aside>
+
+    
+      <h4>Topic 1</h4>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
+        Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+        Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. 
+        Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, 
+        venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus 
+        elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, 
+        eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+         Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam 
+
+
+      </p>
+
+      <h4>Topic 2</h4>
+
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
+        Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+        Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. 
+        Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, 
+        venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus 
+        elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, 
+        eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+         Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam 
+
+
+      </p>
+
+
+      
+
+
+    </aside>
+   
+</div>
+
+<footer>
+    <h2>Source add link to your evidence</h2>
+</footer>
+
+
+
+
+</body>
+</html>
