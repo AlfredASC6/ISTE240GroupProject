ec8dccd2fee5c5fa6e7c58d7e0aa6bbe5cc7a8e2
AlfredASC6 <alfredfrac99@gmail.com>
2022-12-05 14:37:11 -0500

    added connection to database

---
+++ ISTE240GroupProject/inc/config.php
@@ -0,0 +1,16 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'af8857');
+define('DB_PASSWORD', 'Learning4-cerambycidae');
+define('DB_NAME', 'af8857');
+
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
