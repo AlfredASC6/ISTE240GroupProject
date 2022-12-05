fdcc70691b527a7865e2139000c32589331f117f
Natnail Tolossa <ntt7381@g.rit.edu>
2022-12-05 02:25:42 -0500

    head php

--- ISTE240GroupProject/dimarco.php
+++ ISTE240GroupProject/dimarco.php
@@ -1,19 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-<link rel="stylesheet" href="asset/css/styles.css" type = "text/css">
-<link rel="stylesheet" href="asset/css/nav.css" type = "text/css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<script src="asset/js/javascript.js"></script>
+<?php include "inc/head.php" ; ?>
 <title>Nyle</title>
-
-
-
 </head>
+
 <body>
 <?php include "inc/nav.php"; ?>
 
