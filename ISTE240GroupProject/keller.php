e32b30e19a8e7c08326ade85b570335e1d2dd616
AlfredASC6 <52715862+AlfredASC6@users.noreply.github.com>
2022-12-05 17:31:28 -0500

    Add files via upload
    Added the structure for Helen Keller

--- ISTE240GroupProject/keller.php
+++ ISTE240GroupProject/keller.php
@@ -1,43 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-<?php include "inc/head.php" ; ?>
-<title>Thomas Gallaudet</title>
-
-
-
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "style" href = "asset/css/styles.css">
+    <title>Hellen Keller</title>
 </head>
 <body>
-  
-<?php include "inc/nav.php"; ?>
+    <?php include "inc/nav.php" ?>
 
-    <header>  
-        <h1>The Person</h1>
-    
-    </header>
-<div class="row">
-    <main>
-    <img src="asset/image/keller1.jpeg" alt="">
+    <h1>Hellen Keller</h1>
 
-    <br>
-
-    <br>
-
-
-    <br>
-
-    <img src="asset/image/keller3.jpeg" alt="">
-
-   
-    </main>
-    <aside>
-    
-    <h4>Background</h4>
-      <p>
+    <div class = "mid">
+        <img src="asset/image/helenKeller.jpg" alt="Image of Helen Keller">
+        <p>
             Helen Keller, born as Helen Adams Keller was an Americam author, disability rights advocate, political acitvist and lecturer.
             She was born on June 27, 1880 in West Tuscumbia Alabama. Her father was Arthur Henley Keller and her mother was Cathreine
             Everett (Adams) Keller. She had four siblings total and two full siblings: Mildred Campbell (Keller) Tyson and Philip 
@@ -46,28 +23,19 @@
             of the stomach and the brain". The illness left Keller both deaf and blind. She was able to communicate with the 
             daughter of the family cook using her own "home signs". By age 7 she had over 60 home signs that allowed her to 
             communicate with the people in her family and she could also distiguish people by the vibration of their footsteps.
-      </p>
-      <h4>Contribution</h4>
-      <p>
-             On January 22, 1916, Keller and Sullivan traveled to the small town of Menomonie in western Wisconsin 
+        </p>
+    </div>
+
+    <div class = "bottom">
+        <img src="ISTE240GroupProject/asset/image/330px-Helen_KellerA.jpg" alt="">
+        <p>
+            On January 22, 1916, Keller and Sullivan traveled to the small town of Menomonie in western Wisconsin 
             to deliver a lecture at the Mabel Tainter Memorial Building. She had become a world famous speaker and author. She was an 
             advocate of people with disabilities and other numerous causes. She was a sufferage, pacifist, radical socialist, birth 
             control supporter, and opponent of Woodrow Wilson. In 1915 she and Geroge A Kessler founded the HKI (Helen Keller International)
             which devoted its research in nutrition, health and vision. In 1920, Helen Keller also helped found the ACLU (American Civil 
             Liberties Union). She died in her sleep on June 1, 1968 in her home in Connecticuit.   
-      </p>
-      
-    </aside>
-   
-</div>
-<footer>
-    <h2>Reference</h2>
-
-    <p>
-    https://en.wikipedia.org/wiki/Helen_Keller
-
-    </p>
-
-</footer>
+        </p>
+    </div>
 </body>
 </html>
