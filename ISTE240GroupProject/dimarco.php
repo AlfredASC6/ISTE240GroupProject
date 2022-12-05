c265483cdaed63aab1c7d051787d8d845725d811
ajs2019 <118772040+ajs2019@users.noreply.github.com>
2022-12-05 18:26:57 -0500

    Add files via upload

--- ISTE240GroupProject/dimarco.php
+++ ISTE240GroupProject/dimarco.php
@@ -1,17 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-<?php include "inc/head.php" ; ?>
-<title>Nyle</title>
+    <?php include "inc/head.php" ; ?>
+    <title>Nyle Dimarco</title>
 </head>
-
 <body>
-<?php include "inc/nav.php"; ?>
-
+    <?php include "inc/nav.php"; ?>
 
     <header>  
-        <h1>Nyle Di Marco</h1>
+        <h1>Nyle Dimarco</h1>
     
     </header>
 
@@ -19,18 +16,13 @@
 
 
     <main>
-
-     <div class="image" style="height:200px;">Image
-       
-    </div>
-
-
-
+    <img src="asset/image/nyleDiMarco.jpg" alt="alice">
+    <p>Nyle Dimarco: Born in May 8, 1989</p>
      <br>
-
-
-
-     <div class="image" style="height:200px;">Image</div>
+     <br>
+    <img src="asset/image/nyle.jpeg">
+  
+    <br>
 
 
 
@@ -38,51 +30,42 @@
     <aside>
 
     
-      <h4>Topic 1</h4>
+      <h4>Background</h4>
 
       <p>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
-        Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
-        Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. 
-        Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, 
-        venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus 
-        elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, 
-        eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
-         Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam 
-
-
+      Model, actor, and activist Nyle DiMarco, born in Queens, New York City, USA,
+      He grew up with his mom Donna, dad Neal and two brothers, all of whom are also deaf. When DiMarco was a young child, he began to notice changes in his father, who, he says, was struggling with drug and alcohol addiction.
+      DiMarco says he did would also hit him when his mother wasn't home. When he told his mom what was going on, she got a divorce and moved the family to Maryland for a fresh start.
       </p>
+        <br>
+        <br>
+        <br>
 
-      <h4>Topic 2</h4>
+      <h4>Contribution</h4>
 
 
       <p>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
-        Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
-        Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. 
-        Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, 
-        venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus 
-        elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, 
-        eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
-         Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam 
-
-
+      DiMarco does not consider himself disabled by deafness and sees his media profile as an opportunity to bring awareness to Deaf culture. 
+      DiMarco also became the second male winner and first Deaf contestant on the hit CW series.
+      In 2016, DiMarco won season 22 of ABC's dance competition "Dancing with the Stars." With the guidance of professional partner Peta Murgatroyd, 
+      he became the first deaf contestant to take home the coveted Mirror Ball Trophy. Following his DWTS win, DiMarco jumped back in front of the camera, 
+      expanding his acting credits with a role on Hulu's "Difficult People" starring opposite Billy Eichner and John Early. Previously, 
+      he recurred as "Garrett Banducci" on "Switched at Birth" for Freeform, and has a recurring role on Sundance TV's "This Close."
+      DiMarco became a producing member of the 2018 Broadway revival of Mark Medoff's groundbreaking play "Children of a Lesser God." The show, which opened 
+      April 11th at Studio 54, was directed by Tony Award winner Kenny Leon ("A Raisin in the Sun," "Fences") and stars Joshua Jackson ("The Affair"), 
+      Lauren Ridloff ("Wonderstruck"), and Anthony Edwards (ER). This was DiMarco's first producing credit, bringing a unique perspective as a Deaf artist 
+      and invaluable insight to telling such a meaningful story.
       </p>
-
-
-      
-
 
     </aside>
    
 </div>
 
 <footer>
-    <h2>Source add link to your evidence</h2>
+    <h2>Reference:</h2>
+    1. 'Dancing With the Stars' ' Nyle DiMarco Opens Up About Surviving Abuse and Becoming a Deaf Activist <br>
+    2. Why Nyle DiMarco’s Work Is Crucial For The Deaf Community
 </footer>
 
-
-
-
+</section>
 </body>
-</html>
